1f3bbec2ebb6fef682f1bfc2dc7bf7b975640989
elmar <elmar@mediamodifier.com>
2022-07-12 00:30:22 +0300

    compartmentalize

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Error;
+use std::io::{BufReader, Read};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::Sender;
+use std::thread;
+
+pub const BUFFER_LEN: usize = 1024;
+
+// use crate::BUFFER_LEN;
+pub mod utils {
+    pub enum ProgressCommand {
+        SetLength(u64),
+        Inc(),
+        Done(),
+    }
+}
+pub mod server{
+    use super::*;
+    use utils::ProgressCommand;
+    // use crate::BUFFER_LEN;
+
+    pub fn listen(path: String, port: u16, tx: Sender<ProgressCommand>) -> Result<(), Error> {
+        let handle = thread::spawn(move || {
+            let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+            match listener.accept() {
+                Ok((stream, _addr)) => match handle_connection(stream, path, tx) {
+                    Ok(_result) => (),
+                    Err(_err) => println!("Error fetching file."),
+                },
+                Err(e) => println!("couldn't get client: {e:?}"),
+            }
+        });
+        handle.join().expect("Error joining thread.");
+        Ok(())
+    }
+    fn handle_connection(
+        mut stream: TcpStream,
+        path: String,
+        tx: Sender<ProgressCommand>,
+    ) -> Result<(), Error> {
+        let mut file = File::create(path)?;
+        const SIZE_LEN: usize = 8;
+        let mut buff = [0u8; BUFFER_LEN];
+        let mut size_buff = [0u8; SIZE_LEN];
+        let mut bytes_copied: usize;
+        let mut size_reader = BufReader::with_capacity(8, &stream);
+        let mut reader = BufReader::with_capacity(BUFFER_LEN, &stream);
+        size_reader.read(&mut size_buff)?;
+        let units = (u64::from_be_bytes(size_buff) - SIZE_LEN as u64) / BUFFER_LEN as u64;
+        tx.send(ProgressCommand::SetLength(units)).unwrap();
+    
+        loop {
+            tx.send(ProgressCommand::Inc()).unwrap();
+            bytes_copied = reader.read(&mut buff)?;
+            file.write(&buff)?;
+            if bytes_copied < BUFFER_LEN {
+                tx.send(ProgressCommand::Done()).unwrap();
+                break;
+            }
+        }
+        let response = "Transfer successful!";
+    
+        stream.write(response.as_bytes())?;
+        stream.flush()?;
+        Ok(())
+    }
+}
+
+pub mod client {
+    use super::*;
+    use utils::ProgressCommand;
+
+    // no sense in error propagation inside thread, eg ? operator.
+    // handle error inside the thread.
+
+    pub fn send_file(
+        ip: String,
+        file: String,
+        port: u16,
+        tx: Sender<ProgressCommand>,
+    ) -> Result<(), Error> {
+        let handle = thread::spawn(move || {
+            let mut socket = TcpStream::connect(format!("{}:{}", ip, port)).unwrap();
+            let mut bytes_read;
+            let mut file = File::open(file).unwrap();
+            let size = file.metadata().unwrap().len();
+            let units = size as f64 / BUFFER_LEN as f64;
+            let units = units.ceil() as u64;
+            tx.send(ProgressCommand::SetLength(units)).unwrap();
+            socket.write(&size.to_be_bytes()).unwrap();
+            let mut buffer = [0u8; BUFFER_LEN];
+            loop {
+                tx.send(ProgressCommand::Inc()).unwrap();
+                bytes_read = file.read(&mut buffer).unwrap();
+                socket.write(&buffer).unwrap();
+                if bytes_read != BUFFER_LEN {
+                    tx.send(ProgressCommand::Done()).unwrap();
+                    break;
+                }
+            }
+        });
+        handle.join().unwrap();
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 use clap::Parser;
 use indicatif::ProgressBar;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Error;
-use std::io::{BufReader, Read};
-use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::Arc;
-use std::thread;
+use filer::utils::ProgressCommand;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -25,12 +20,6 @@ struct Args {
     port: u16,
 }
 
-enum ProgressCommand {
-    SetLength(u64),
-    Inc(),
-    Done(),
-}
-
 ///
 /// File transfer tool.
 /// To send a file, provide an IP address and a path to a file.
@@ -42,28 +31,20 @@ fn main() {
     let args = Args::parse();
     let bar = Arc::new(ProgressBar::new(1));
 
-    let handle;
     let (tx, rx): (Sender<ProgressCommand>, Receiver<ProgressCommand>) = mpsc::channel();
     if let (Some(ip), Some(file)) = (args.ip, args.file) {
         // client
-        handle = thread::spawn(move || match send_file(&ip, &file, &args.port, tx) {
-            Ok(_res) => (),
-            Err(_err) => println!("Could not send file."),
-        });
+        match filer::client::send_file(ip, file, args.port, tx) {
+            Ok(()) => (),
+            Err(err) => println!("Failed sending file. {err}"),
+        };
     } else if let Some(path) = args.path {
         {
             // server
-
-            handle = thread::spawn(move || {
-                let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).unwrap();
-                match listener.accept() {
-                    Ok((stream, _addr)) => match handle_connection(stream, path, tx) {
-                        Ok(_result) => (),
-                        Err(_err) => println!("Error fetching file."),
-                    },
-                    Err(e) => println!("couldn't get client: {e:?}"),
-                }
-            })
+            match filer::server::listen(path, args.port, tx) {
+                Ok(()) => (),
+                Err(err) => println!("Error receiving file. {err}"),
+            };
         }
     } else {
         println!("See help for instructions.");
@@ -76,75 +57,74 @@ fn main() {
             ProgressCommand::Done() => bar.finish(),
         }
     }
-    handle.join().unwrap();
     println!("Done!");
 }
 
-#[inline(always)]
-fn _use_receive_mode(ip: &Option<String>, file: &Option<String>) -> bool {
-    if let (Some(_ip), Some(_file)) = (ip, file) {
-        return true;
-    }
-    false
-}
+// #[inline(always)]
+// fn _use_receive_mode(ip: &Option<String>, file: &Option<String>) -> bool {
+//     if let (Some(_ip), Some(_file)) = (ip, file) {
+//         return true;
+//     }
+//     false
+// }
 
-fn send_file(
-    ip: &String,
-    file: &String,
-    port: &u16,
-    tx: Sender<ProgressCommand>,
-) -> Result<(), Error> {
-    let mut socket = TcpStream::connect(format!("{}:{}", ip, port))?;
-    let mut bytes_read;
-    let mut file = File::open(file)?;
-    let size = file.metadata()?.len();
-    const BUFFER_LEN: usize = 1024;
-    let units = size as f64 / BUFFER_LEN as f64;
-    let units = units.ceil() as u64;
-    tx.send(ProgressCommand::SetLength(units)).unwrap();
-    socket.write(&size.to_be_bytes())?;
-    let mut buffer = [0u8; BUFFER_LEN];
-    loop {
-        tx.send(ProgressCommand::Inc()).unwrap();
-        bytes_read = file.read(&mut buffer)?;
-        socket.write(&buffer)?;
-        if bytes_read != BUFFER_LEN {
-            tx.send(ProgressCommand::Done()).unwrap();
-            break;
-        }
-    }
-    return Ok(());
-}
+// fn send_file(
+//     ip: &String,
+//     file: &String,
+//     port: &u16,
+//     tx: Sender<ProgressCommand>,
+// ) -> Result<(), Error> {
+//     let mut socket = TcpStream::connect(format!("{}:{}", ip, port))?;
+//     let mut bytes_read;
+//     let mut file = File::open(file)?;
+//     let size = file.metadata()?.len();
+//     const BUFFER_LEN: usize = 1024;
+//     let units = size as f64 / BUFFER_LEN as f64;
+//     let units = units.ceil() as u64;
+//     tx.send(ProgressCommand::SetLength(units)).unwrap();
+//     socket.write(&size.to_be_bytes())?;
+//     let mut buffer = [0u8; BUFFER_LEN];
+//     loop {
+//         tx.send(ProgressCommand::Inc()).unwrap();
+//         bytes_read = file.read(&mut buffer)?;
+//         socket.write(&buffer)?;
+//         if bytes_read != BUFFER_LEN {
+//             tx.send(ProgressCommand::Done()).unwrap();
+//             break;
+//         }
+//     }
+//     return Ok(());
+// }
 
-fn handle_connection(
-    mut stream: TcpStream,
-    path: String,
-    tx: Sender<ProgressCommand>,
-) -> Result<(), Error> {
-    let mut file = File::create(path)?;
-    const BUFFER_LEN: usize = 1024;
-    const SIZE_LEN: usize = 8;
-    let mut buff = [0u8; BUFFER_LEN];
-    let mut size_buff = [0u8; SIZE_LEN];
-    let mut bytes_copied: usize;
-    let mut size_reader = BufReader::with_capacity(8, &stream);
-    let mut reader = BufReader::with_capacity(BUFFER_LEN, &stream);
-    size_reader.read(&mut size_buff)?;
-    let units = (u64::from_be_bytes(size_buff) - SIZE_LEN as u64) / BUFFER_LEN as u64;
-    tx.send(ProgressCommand::SetLength(units)).unwrap();
+// fn handle_connection(
+//     mut stream: TcpStream,
+//     path: String,
+//     tx: Sender<ProgressCommand>,
+// ) -> Result<(), Error> {
+//     let mut file = File::create(path)?;
+//     const BUFFER_LEN: usize = 1024;
+//     const SIZE_LEN: usize = 8;
+//     let mut buff = [0u8; BUFFER_LEN];
+//     let mut size_buff = [0u8; SIZE_LEN];
+//     let mut bytes_copied: usize;
+//     let mut size_reader = BufReader::with_capacity(8, &stream);
+//     let mut reader = BufReader::with_capacity(BUFFER_LEN, &stream);
+//     size_reader.read(&mut size_buff)?;
+//     let units = (u64::from_be_bytes(size_buff) - SIZE_LEN as u64) / BUFFER_LEN as u64;
+//     tx.send(ProgressCommand::SetLength(units)).unwrap();
 
-    loop {
-        tx.send(ProgressCommand::Inc()).unwrap();
-        bytes_copied = reader.read(&mut buff)?;
-        file.write(&buff)?;
-        if bytes_copied < BUFFER_LEN {
-            tx.send(ProgressCommand::Done()).unwrap();
-            break;
-        }
-    }
-    let response = "Transfer successful!";
+//     loop {
+//         tx.send(ProgressCommand::Inc()).unwrap();
+//         bytes_copied = reader.read(&mut buff)?;
+//         file.write(&buff)?;
+//         if bytes_copied < BUFFER_LEN {
+//             tx.send(ProgressCommand::Done()).unwrap();
+//             break;
+//         }
+//     }
+//     let response = "Transfer successful!";
 
-    stream.write(response.as_bytes())?;
-    stream.flush()?;
-    Ok(())
-}
+//     stream.write(response.as_bytes())?;
+//     stream.flush()?;
+//     Ok(())
+// }
